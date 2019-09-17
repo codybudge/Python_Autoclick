4cf19df38be846f21a779f9cd3c5dc783f61069d
cody budge <codybudge@gmail.com>
2019-09-17 14:26:28 -0600

    ran print test for version and text

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
 import sys
 
-print
+print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    greeting = 'hello, {}'.format(who_to_greet)
+    return greeting
+
+    print(greet('World'))
+    print(greet('Cody'))
